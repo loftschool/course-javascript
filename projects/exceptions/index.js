be08c0c792d7ca56e0c70af4f9eff081d922bb23
Ruslan Kinzebaev <less777i@gmail.com>
2021-05-03 16:58:45 +0500

    homework 3.1

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -17,7 +17,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || array.length < 1) {
+  if (array.constructor !== Array || array.length < 1) {
     throw new Error('empty array');
   } else if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
@@ -47,7 +47,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || array.length < 1) {
+  if (array.constructor !== Array || array.length < 1) {
     throw new Error('empty array');
   } else if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
