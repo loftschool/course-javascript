c2b997cad3311ce1471de888a675dfca4e48387c
Анастасия Тишина <anastasia@MacBook-Pro-Anastasia.local>
2021-05-12 22:46:35 +0300

    ДЗ 6 - DOM Events

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -8,7 +8,9 @@
  Пример:
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
-function addListener(eventName, target, fn) {}
+function addListener(eventName, target, fn) {
+  target.addEventListener(eventName, fn);
+}
 
 /*
  Задание 2:
@@ -18,7 +20,9 @@ function addListener(eventName, target, fn) {}
  Пример:
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
-function removeListener(eventName, target, fn) {}
+function removeListener(eventName, target, fn) {
+  target.removeEventListener(eventName, fn);
+}
 
 /*
  Задание 3:
@@ -28,8 +32,12 @@ function removeListener(eventName, target, fn) {}
  Пример:
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
-function skipDefault(eventName, target) {}
 
+function skipDefault(eventName, target) {
+  target.addEventListener(eventName, function () {
+    eventName.preventDefault();
+  });
+}
 /*
  Задание 4:
 
@@ -49,7 +57,14 @@ function emulateClick(target) {}
  Пример:
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
-function delegate(target, fn) {}
+
+function delegate(target, fn) {
+  target.addEventListener('click', function (e) {
+    if (e.target.tagName === 'BUTTON') {
+      fn();
+    }
+  });
+}
 
 /*
  Задание 7:
