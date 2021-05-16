60540a84679d713881d567f5783c9761bed92b9c
Ruslan Kinzebaev <less777i@gmail.com>
2021-05-16 12:38:46 +0500

    homework 6.1

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -18,18 +18,41 @@
 import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
+let mouseOffset;
+let dragElem;
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousemove', (e) => {
+  e.preventDefault();
+});
+document.addEventListener('mousedown', (e) => {
+  if (e.target.classList.contains('draggable-div')) {
+    dragElem = e.target;
+    const pos = getPosition(e.target);
+    mouseOffset = {
+      x: e.pageX - pos.x,
+      y: e.pageY - pos.y,
+    };
+  }
+});
+document.addEventListener('mouseup', (e) => {
+  if (dragElem) {
+    dragElem.style.top = e.pageY - mouseOffset.y + 'px';
+    dragElem.style.left = e.pageX - mouseOffset.x + 'px';
+    dragElem = null;
+  }
+});
 
 export function createDiv() {
   const elem = document.createElement('div');
   elem.classList.add('draggable-div');
+  const maxHeight = 200;
+  const maxWidth = 400;
   const windowHeight = window.outerHeight;
   const windowWidth = window.outerWidth;
-  const elemHeight = Math.random() * windowHeight;
-  const elemWidth = Math.random() * windowWidth;
-  const elemTop = Math.random() * (windowHeight - elemHeight);
-  const elemLeft = Math.random() * (windowWidth - elemWidth);
+  const elemHeight = Math.random() * maxHeight;
+  const elemWidth = Math.random() * maxWidth;
+  const elemTop = Math.random() * (windowHeight - maxHeight);
+  const elemLeft = Math.random() * (windowWidth - maxWidth);
   const elemBackgroundColor = Math.floor(Math.random() * 16777215).toString(16);
   elem.style.cssText = 'height:' + elemHeight + 'px; width:' + elemWidth + 'px;';
   elem.style.cssText += 'top:' + elemTop + 'px; left:' + elemLeft + 'px;';
@@ -45,3 +68,17 @@ addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
 });
+
+function getPosition(e) {
+  let left = 0;
+  let top = 0;
+  while (e.offsetParent) {
+    left += e.offsetLeft;
+    top += e.offsetTop;
+    e = e.offsetParent;
+  }
+  left += e.offsetLeft;
+  top += e.offsetTop;
+
+  return { x: left, y: top };
+}
