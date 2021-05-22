24cff4cc9d6b228ae38aa0e5f96c351e2e0d3832
Vladislav Kitaev <vladislavkitaev@MacBook-Pro-Vladislav.local>
2021-05-22 11:00:20 +0300

    готовые дз

--- projects/hw1/index.js
+++ projects/hw1/index.js
@@ -87,7 +87,14 @@ function returnCounter(num) {
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  // eslint-disable-next-line no-var
+  var arr = [];
+  for (let i = 0; i < arguments.length; i++) {
+    arr.push(arguments[i]);
+  }
+  return arr;
+}
 
 /*
  Задание 6 *:
@@ -104,7 +111,17 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn, ...args) {}
+function bindFunction(fn, ...args) {
+  // eslint-disable-next-line no-var
+  var arr = [];
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] != 'function') {
+      arr.push(arguments[i]);
+    }
+  }
+  const newFn = fn.bind(null, ...arr);
+  return newFn;
+}
 
 export {
   returnFirstArgument,
