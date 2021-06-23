de2e7dbc38a7f2cd14f565a10d0f961d9c8114b9
Fannur Khusnullin <notait34@gmail.com>
2021-06-23 23:33:49 +0300

    сделал обязаетльные задания

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,15 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const arrayToReturn = [];
+
+  for (let i = 0; i < array.length; i++) {
+    arrayToReturn[i] = fn(array[i], i, array);
+  }
+
+  return arrayToReturn;
+}
 
 /*
  Задание 3:
@@ -31,7 +43,14 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let accumulator = initial === undefined ? array[0] : initial;
+  const start = initial === undefined ? 1 : 0;
+  for (let i = start; i < array.length; i++) {
+    accumulator = fn(accumulator, array[i], i, array);
+  }
+  return accumulator;
+}
 
 /*
  Задание 4:
@@ -41,7 +60,13 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const array = [];
+  for (const key in obj) {
+    array.push(key.toUpperCase());
+  }
+  return array;
+}
 
 /*
  Задание 5 *:
