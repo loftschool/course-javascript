afd87e729015b7f7f7134824796a0bad3d2f9ee6
Dadaev Abdulazym <adadaev2016@gmail.com>
2021-06-28 00:20:55 +0300

    2 h/w done

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -10,7 +10,7 @@
    forEach([1, 2, 3], (el) => console.log(el))
  */
 function forEach(array, fn) {
-  for (let i = 0; i < array.lenght; i++) {
+  for (let i = 0; i < array.length; i++) {
     fn(array[i], i, array);
   }
 }
@@ -26,7 +26,7 @@ function forEach(array, fn) {
  */
 function map(array, fn) {
   const new_array = [];
-  for (let i = 0; i < array.lenght; i++) {
+  for (let i = 0; i < array.length; i++) {
     new_array[i] = fn(array[i], i, array);
   }
   return new_array;
@@ -44,7 +44,7 @@ function reduce(array, fn, initial) {
   const hasInitial = typeof initial !== 'undefined';
   let prev = hasInitial ? initial : array[0];
 
-  for (let i = hasInitial ? 0 : 1; i < array.lenght; i++) {
+  for (let i = hasInitial ? 0 : 1; i < array.length; i++) {
     prev = fn(prev, array[i], i, array);
   }
 
@@ -63,7 +63,7 @@ function upperProps(obj) {
   const new_array = [];
 
   // eslint-disable-next-line no-undef
-  for (item in obj) {
+  for (const item in obj) {
     // new_array.push(obj.item.toUpperCase());
     // eslint-disable-next-line no-undef
     new_array.push(item.toUpperCase());
@@ -85,7 +85,7 @@ function upperProps(obj) {
  */
 function createProxy(obj) {
   // eslint-disable-next-line no-unused-vars
-  const proxy = new Proxy(obj, {
+  return new Proxy(obj, {
     set(obj, prop, val) {
       obj[prop] = val ** 2;
       return true;
