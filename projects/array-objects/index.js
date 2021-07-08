265d932b5539c22e7ab7682b0743ee4f7f6c4c46
lebedevpa <p.lebedev88@gmail.com>
2021-07-08 12:16:21 +0300

    Update index.js

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -53,7 +53,7 @@ function reduce(array, fn, initial) {
   const hasInitial = typeof initial !== 'undefined';
   let prev = hasInitial ? initial : array[0];
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = hasInitial ? 0 : 1; i < array.length; i++) {
     prev = fn(prev, array[i], i, array);
   }
 
