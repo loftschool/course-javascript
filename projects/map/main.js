9487ec983a8f4dd7ec9bbc8961bda0c4fc78e6d3
Ekaterina <ek.chasovskikh94@gmail.com>
2021-07-20 12:21:23 +0300

    Карта

---
+++ projects/map/main.js
@@ -0,0 +1,12 @@
+// import './index.html';
+
+// ymaps.ready(init);
+
+// function init() {
+// 	var map = new ymaps.Map('map', {
+// 		center: [59.94, 30.32],
+// 		zoom: 12,
+// 		controls: ['zoomControl'],
+// 		behaviors: ['drag']
+// 	});
+// }
