2e3e78eb375bb05b118dd52ebf32361bc0fe2a6d
Gainullin Kirill <gaynullinkh@gmail.com>
2021-08-01 12:36:10 +0300

    DZ3-2

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -21,7 +21,7 @@ function isAllTrue(array, fn) {
     throw new Error("fn is not a function");
   }
 
-  if (array.length == 0 || !Array.isArray(array)) {
+  if (array.length == 0 || array.constructor !== Array) {
     throw new Error("empty array");
   }
   
@@ -54,7 +54,7 @@ function isSomeTrue(array, fn) {
     throw new Error("fn is not a function");
   }
 
-  if (array.length == 0 || !Array.isArray(array)) {
+  if (array.length == 0 || array.constructor !== Array) {
     throw new Error("empty array");
   } 
 
