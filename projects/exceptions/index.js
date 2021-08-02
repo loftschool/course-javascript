d2667526738ca8abc8f9c069d2633e9ef9310d3c
Илья Гулида <ilagulida@MacBook-Pro-Ila.local>
2021-08-02 20:13:23 +0300

    workout3

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -21,7 +21,7 @@ function isAllTrue(array, fn) {
     throw new Error('fn is not a function');
   }
 
-  if (!Array.isArray(array) || !array.length) {
+  if (array.constructor.name !== 'Array' || !array.length) {
     throw new Error('empty array');
   }
 
@@ -55,7 +55,7 @@ function isSomeTrue(array, fn) {
     throw new Error('fn is not a function');
   }
 
-  if (!Array.isArray(array) || !array.length) {
+  if (array.constructor.name !== 'Array' || !array.length) {
     throw new Error('empty array');
   }
 
