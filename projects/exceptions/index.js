7fd991ce85a08e65f0d34bb27d668b0f2c435855
Yarashevich Yauheni <jerichyar@gmail.com>
2021-08-03 22:51:21 +0200

    Задание 3

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,20 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+  for (const el of array) {
+    if (!fn(el)) {
+      return false;
+    }
+  }
+  return true;
+}
 
 /*
  Задание 2:
@@ -34,7 +47,20 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+  for (const el of array) {
+    if (fn(el)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /*
  Задание 3:
@@ -47,7 +73,20 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  const bad = [];
+  for (const arg of args) {
+    try {
+      fn(arg);
+    } catch {
+      bad.push(arg);
+    }
+  }
+  return bad;
+}
 
 /*
  Задание 4:
@@ -66,7 +105,44 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (!Number.isFinite(number)) {
+    throw new Error('number is not a number');
+  }
+  return {
+    sum(...args) {
+      let result = number;
+      for (const arg of args) {
+        result += arg;
+      }
+      return result;
+    },
+    dif(...args) {
+      let result = number;
+      for (const arg of args) {
+        result -= arg;
+      }
+      return result;
+    },
+    div(...args) {
+      let result = number;
+      for (const arg of args) {
+        if (arg === 0) {
+          throw new Error('division by 0');
+        }
+        result /= arg;
+      }
+      return result;
+    },
+    mul(...args) {
+      let result = number;
+      for (const arg of args) {
+        result *= arg;
+      }
+      return result;
+    },
+  };
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
