f53ffb3b1e751d83ff90dde129e5527b8958194b
Gainullin Kirill <gaynullinkh@gmail.com>
2021-08-17 17:08:57 +0300

    DZ6-2

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -76,7 +76,14 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', fn, { once: true });
+  let already = false;
+
+  target.addEventListener('click', () => {
+    if (!already) {
+      fn();
+      already = true;
+    }
+  });
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
