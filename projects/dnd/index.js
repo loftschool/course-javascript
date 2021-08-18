9a2e4e2becfd95ae1928cea09b6e734534cc9578
Yarashevich Yauheni <jerichyar@gmail.com>
2021-08-19 01:44:01 +0200

    Задание 6

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -17,15 +17,50 @@
  */
 import './dnd.html';
 
-const homeworkContainer = document.querySelector('#app');
+const homeworkContainer = document.querySelector('#homework-container');
 
-document.addEventListener('mousemove', (e) => {});
+function random(from, to) {
+    return parseInt(from + Math.random() * to - from);
+}
 
-export function createDiv() {}
+let currentDrag;
+let startX = 0;
+let startY = 0;
+
+
+document.addEventListener('mousemove', (e) => {
+    if (currentDrag) {
+        currentDrag.style.top = e.clientY - startY + 'px';
+        currentDrag.style.left = e.clientx - startX + 'px';
+    }
+});
+
+export function createDiv() {
+    const div = document.createElement('div');
+    const minSize = 20;
+    const maxSize = 200;
+    const maxColor = 0xffffff;
+
+    div.className = 'draggable-div';
+    div.style.background = '#' + random(0, maxColor).toString(16);
+    div.style.top = random(0, window.innerHeight) + 'px';
+    div.style.left = random(0, window.innerWidth) + 'px';
+    div.style.width = random(minSize, maxSize) + 'px';
+    div.style.height = random(minSize, maxSize) + 'px';
+
+    div.addEventListener('mousedown', (e) => {
+        currentDrag = div;
+        startX = e.offsetX;
+        startY = e.offsetY;
+    });
+    div.addEventListener('mouseup', () => (currentDrag = false));
+
+    return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function () {
-  const div = createDiv();
-  homeworkContainer.appendChild(div);
+addDivButton.addEventListener('click', function() {
+    const div = createDiv();
+    homeworkContainer.appendChild(div);
 });
