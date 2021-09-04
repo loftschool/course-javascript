39f063edafebf334a096069363c4773a475c4c22
theCount <thescheremetev@mail.ru>
2021-09-04 11:45:38 +0300

    I completed second task

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -76,6 +76,13 @@ function upperProps(obj) {
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set(obj, key, value) {
+      obj[key] = value * value;
+      return true;
+    },
+  });
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
