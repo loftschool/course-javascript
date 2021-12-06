bd13ad7eca350a6b67ccd378f198ac16b0385cd8
Artem Archenkov <r203mail@gmail.com>
2021-12-06 18:32:38 +0400

    GEO check

--- projects/geo/index.js
+++ projects/geo/index.js
@@ -6,6 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
   let coords = [];
   let reviews = [];
 
+  const storage = {
+    add(obj) {
+      let temp = localStorage.geo ? JSON.parse(localStorage.geo) : [];
+      temp.push(obj);
+      localStorage.geo = JSON.stringify(temp);
+    },
+    getAll() {
+      return localStorage.geo ? JSON.parse(localStorage.geo) : [];
+    }
+  }
+
 
   const balloonContent = [
       '<div class="review-form">',
@@ -33,7 +44,34 @@ document.addEventListener('DOMContentLoaded', function() {
       const clusterer = new ymaps.Clusterer({
         hasBalloon: false,
         clusterDisableClickZoom: true,
+        groupByCoordinates: true
       });
+
+      storage.getAll().forEach((item) => {
+        reviews.push(item);
+        let placemark = new ymaps.Placemark([item.latitude, item.longitude], {});
+        clusterer.add(placemark);
+        myMap.geoObjects.add(clusterer);
+
+        placemark.events.add('click', function(e) {
+          openBallon(e);
+        });
+
+        clusterer.events.add('click', function (e) {
+          openBallon(e);
+        });
+      });
+
+      function openBallon(e) {
+        let [currentLatitude, currentLongitude] = e.get('target').geometry.getCoordinates();
+        coords = e.get('target').geometry.getCoordinates();
+
+        const findReviews = reviews.filter(review => currentLatitude === review.latitude) || (currentLongitude === review.longitude);
+        const html = reviewTemplate({findReviews});
+        myMap.balloon.open(e.get('target').geometry.getCoordinates(), {
+          contentBody: html + balloonContent
+        });
+      }
   
       myMap.events.add('click', function (e) {
         coords = e.get('coords');
@@ -54,39 +92,20 @@ document.addEventListener('DOMContentLoaded', function() {
                   longitude: coords[1]
                 };
 
+              storage.add(review);
               reviews.push(review);
+
               let placemark = new ymaps.Placemark([coords[0], coords[1]], {});
               clusterer.add(placemark);
               myMap.geoObjects.add(clusterer);
               myMap.balloon.close();
 
               placemark.events.add('click', function(e) {
-
-                // console.log(e.get('target'));
-                // let [currentLatitude, currentLongitude] = placemark.geometry.getCoordinates();
-              //   const findReviews = reviews.filter(review => currentLatitude === review.latitude) || (currentLongitude === review.longitude)
-                
-              //   const html = reviewTemplate({findReviews});
-
-              //   myMap.balloon.open([currentLatitude, currentLongitude], {
-              //     contentBody: html + balloonContent + 'placemark'
-              //   });
+                openBallon(e);
               })
 
               clusterer.events.add('click', function (e) {
-                let geoObjectState = clusterer.getObjectState(placemark)
-                console.log(geoObjectState.cluster);
-                // console.log(e.get('target').geometry.getCoordinates());
-                // console.log(reviews);
-                // let [currentLatitude, currentLongitude] = placemark.geometry.getCoordinates();
-                let [currentLatitude, currentLongitude] = placemark.geometry.getCoordinates();
-
-                const findReviews = reviews.filter(review => currentLatitude === review.latitude) || (currentLongitude === review.longitude);
-                // console.log(findReviews);
-                const html = reviewTemplate({findReviews});
-                myMap.balloon.open(e.get('target').geometry.getCoordinates(), {
-                  contentBody: html + balloonContent + 'cluster'
-                });
+                openBallon(e);
               });
               
               
