74983688b380ad74251d9db7bbe86859f31a8933
sinnderwelt <dmitri.radajev@gmail.com>
2021-12-18 12:26:17 +0200

    Сделал второе задание

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,14 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const array1 = [1, 2, 3, 4];
+
+  const result = array1.map(function (elem) {
+    return elem * elem;
+  });
+  console.log(result);
+}
 
 /*
  Задание 3:
@@ -31,7 +42,15 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  const arr = [1, 2, 3];
+
+  const result = arr.reduce(function (a, b) {
+    return a + b;
+  });
+
+  console.log(result);
+}
 
 /*
  Задание 4:
@@ -41,7 +60,16 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const obje = { name: 'Сергей', lastName: 'Петров' };
+  const test = Object.keys(obje);
+  const arrToUp = String.prototype.toUpperCase.apply(test).split(',');
+
+  console.log(arrToUp);
+  return obj;
+}
+
+upperProps();
 
 /*
  Задание 5 *:
