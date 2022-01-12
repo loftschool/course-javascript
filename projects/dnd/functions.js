f930d25154e5b6abd6f9e18096ac480c0d63f154
ratd <dmitrii_r@list.ru>
2022-01-12 19:57:54 +0300

    поправил функ once

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -78,7 +78,13 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', fn,{ once: true });
+  let counter = 0;
+  target.addEventListener('click', (e) => {
+    if (counter === 0) {
+      fn();
+    }
+    counter++;
+  });
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
