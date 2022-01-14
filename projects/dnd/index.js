b9fdf8d19532f14e30b74738f187c9fb92471453
ratd <dmitrii_r@list.ru>
2022-01-14 14:53:13 +0300

    draggable-div

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,62 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousemove', (e) => {
+  document.body.style.cursor = 'pointer';
+});
 
-export function createDiv() {}
+function randomSize(from, to) {
+  return parseInt(from + Math.random() * to - from);
+}
+
+function randomColor() {
+  let r, g, b;
+  r = randomSize(0, 255);
+  g = randomSize(0, 255);
+  b = randomSize(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
+
+function randomPosition() {
+  let maxX = screen.availWidth;
+  let maxY = screen.availHeight;
+  let startX = window.screenX;
+  let startY = window.screenY;
+  let position = {};
+  position.x = randomSize(startX, maxX);
+  position.y = randomSize(startY, maxY);
+  return position;
+}
+
+export function createDiv() {
+  const div = document.createElement('div');
+  let maxWidth = 100;
+  let maxHeight = 100;
+  let position = randomPosition();
+  div.classList.add("draggable-div");
+  div.style.backgroundColor = randomColor();
+  div.style.width = randomSize(1, maxWidth) + 'px';
+  div.style.height = randomSize(1, maxHeight) + 'px';
+  div.style.top = position.y + 'px';
+  div.style.left = position.x + 'px';
+  div.setAttribute("draggable", "true");
+  let getCoords = (elem) => {
+    let box = elem.getBoundingClientRect();
+
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  }
+  div.addEventListener("dragend", (event) => {
+    let coords = getCoords(event.target),
+      x = event.pageX,
+      y = event.pageY;
+    div.style.top = y + "px";
+    div.style.left = +x + "px";
+  });
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
