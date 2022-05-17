a6e06abb875dcf1e2bd349ec19a2efdcceb7297a
oleg <kot-pirat@bk.ru>
2022-05-17 18:13:27 +0300

    Исправления

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -17,7 +17,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || array.length === 0) {
+  if (!(array instanceof Array) || array.length === 0) {
     throw new Error('empty array');
   }
   if (typeof fn !== 'function') {
@@ -50,7 +50,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || array.length === 0) {
+  if (!(array instanceof Array) || array.length === 0) {
     throw new Error('empty array');
   }
   if (typeof fn !== 'function') {
