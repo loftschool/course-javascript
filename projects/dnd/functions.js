c4e112e25c3142a316b15c6cc6130fd6004fd907
Stanislav Otmarshtein <stanislavshina@yandex.ru>
2022-07-05 18:27:54 +0500

    внес правки

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -77,7 +77,10 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', fn, { once: true });
+  target.addEventListener('click', function deleteAfterFirst() {
+    fn();
+    target.removeEventListener('click', deleteAfterFirst);
+  });
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
