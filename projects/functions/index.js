56d7362c7bc391f57460b0122953eda1a80c5007
Vladimir <melnikov.v@office.lan>
2023-03-01 22:15:16 +0300

    week-1: Выполнены задания 1-5

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,14 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(foo) {
+  return foo;
+}
+
+console.group('Task 1');
+console.log(returnFirstArgument(10));
+console.log(returnFirstArgument('привет'));
+console.groupEnd();
 
 /*
  Задание 2:
@@ -28,7 +35,15 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
+
+console.group('Task 2');
+console.log(sumWithDefaults(10, 20));
+console.log(sumWithDefaults(2, 4));
+console.log(sumWithDefaults(2));
+console.groupEnd();
 
 /*
  Задание 3:
@@ -38,7 +53,14 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(fn) {
+  return fn();
+}
+
+console.group('Task 3');
+console.log(returnFnResult(() => 'привет'));
+console.log(returnFnResult(() => 2 * 2));
+console.groupEnd();
 
 /*
  Задание 4:
@@ -53,7 +75,27 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(num = 0) {
+  let n = num;
+
+  function inc() {
+    n += 1;
+    return n;
+  }
+
+  return inc;
+}
+
+const f = returnCounter(10);
+const k = returnCounter();
+
+console.group('Task 4');
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(k());
+console.log(k());
+console.groupEnd();
 
 /*
  Задание 5 *:
@@ -64,7 +106,13 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+  return args;
+}
+
+console.group('Task 5');
+console.group(returnArgumentsArray(1, 2, 3));
+console.groupEnd();
 
 export {
   returnFirstArgument,
