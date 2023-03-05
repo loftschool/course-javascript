98d883ddf4406418e642b03de703b3d88a70855b
Sergey S <paniqwow@gmail.com>
2023-03-05 13:36:18 +0300

    hw w1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,8 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
-
+function returnFirstArgument(arg) {
+  return arg;
+}
 /*
  Задание 2:
 
@@ -28,8 +29,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
-
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 /*
  Задание 3:
 
@@ -38,7 +40,11 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+
+function returnFnResult(bar) {
+  return bar();
+}
+
 
 /*
  Задание 4:
@@ -53,7 +59,9 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(i = 0) {
+  return () => ++i;
+}
 
 /*
  Задание 5 *:
@@ -64,7 +72,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+  return args;
+}
 
 export {
   returnFirstArgument,
