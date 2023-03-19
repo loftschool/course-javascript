cfac7ba43c873477b3f76cc1c88df417881cd855
Diana Nagimova <118924554+DianaN7@users.noreply.github.com>
2023-03-19 12:19:50 +0300

    Delete projects directory

--- projects/loft-photo-lite-2/index.js
+++
@@ -1,8 +0,0 @@
-import pages from './pages';
-import('./styles.css');
-
-const pageNames = ['login', 'main', 'profile'];
-
-document.addEventListener('click', () => {
-  pages.openPage('main');
-});
