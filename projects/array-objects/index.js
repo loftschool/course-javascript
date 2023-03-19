3c4b7496dc6204e6a2bee063111242a6b1790026
Aleksandr Kamardin <sashka-stalker93@mail.ru>
2023-03-19 13:21:03 +0300

    Homework n.2 second commit

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,18 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+  
+
+  // for (const [ix, el] of array.entries()) {
+  //   fn(el, ix, array);
+  // }
+}
+
+forEach([1, 2, 3], (el) => console.log(el))
 
 /*
  Задание 2:
@@ -21,7 +32,17 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+function map(array, fn) {
+  const newArray = [];
+
+  for(const [ix, el] of array.entries()) {
+    let newEl = fn(el, ix, array)
+    newArray.push(newEl)
+  }
+
+  return newArray;
+  // console.log(newArray)
+}
 
 /*
  Задание 3:
@@ -33,7 +54,22 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+function reduce(array, fn, initialValue) {
+  let startInd = 0;
+  let all = initialValue;
+
+    if (initialValue === undefined) {
+      startInd = 1;
+      all = array[0];
+    }
+
+  for(let i = startInd; i < array.length; i++) {
+    all = fn(all, array[i], i, array);
+  }
+
+return all
+
+}
 
 /*
  Задание 4:
@@ -44,6 +80,11 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+function upperProps(object) {
+  let upperCase;
+  upperCase = Object.keys(object).map(a => a.toUpperCase());
+
+  return upperCase;
+}
 
 export { forEach, map, reduce, upperProps };
--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -4,6 +4,34 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if(!Array.isArray(array) || array.length === 0) {
+      throw new Error ('empty array');
+    }
+
+    const ix = parseInt(Math.random() * (array.length - 1));
+
+    return array[ix];
+
+    // if(!array.length) {
+    //   return null;
+    // }
+
+  },
+
+
+
+  getNextPhoto(array, ) {
+
+    const friend = this.getRandomElement(friendsDB);
+
+    const photos = photosDB[friend, id];
+
+    const photo = this.getRandomElement(photos);
+
+    return {
+      friend,
+      url: photo.url
+    };
+  },
 };
