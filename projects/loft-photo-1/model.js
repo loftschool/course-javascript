53abf6a08ba0f5c5d24d29e8ba3ddcb33e531f38
hellkapwnz <104258955+hellkapwnz@users.noreply.github.com>
2023-03-21 09:07:49 +0300

    Update model.js

--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -4,6 +4,20 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!array.lenght) {
+      return null;
+    }
+
+    const index = Math.round(Math.random() * (array.lenght - 1));
+
+    return array[index];
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB.get(friend.id);
+    const photo = this.getRandomElement(photos);
+
+    return { friend, url: photo.url};
+  },
 };
