c6beac52ed208f73fec2bae297d321a14ce5aba5
Sergey Melyukov <smelukov@yandex-team.ru>
2023-04-17 19:06:20 +0300

    fix

--- projects/loft-photo/model.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import photosDB from './photos.json';
-// eslint-disable-next-line no-unused-vars
-import friendsDB from './friends.json';
-
-export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
-};
