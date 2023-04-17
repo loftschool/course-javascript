102c0933d1a748465240193c33fcb66bedf06375
Sergey Melyukov <s.melukov@gmail.com>
2023-04-17 19:08:13 +0300

    loft photo

---
+++ projects/loft-photo/model.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import photosDB from './photos.json';
+// eslint-disable-next-line no-unused-vars
+import friendsDB from './friends.json';
+
+export default {
+  getRandomElement(array) {},
+  getNextPhoto() {},
+};
