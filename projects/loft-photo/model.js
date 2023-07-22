f64f6b8e0a8e4c82e1f4485cedc2b10a5b62baf4
Polina <109180069+JimmyCoid@users.noreply.github.com>
2023-07-22 12:32:20 +0300

    Функции `getRandomElement(array)` и `getNextPhoto()`

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,9 +1,22 @@
-// eslint-disable-next-line no-unused-vars
 import photosDB from './photos.json';
-// eslint-disable-next-line no-unused-vars
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+    getRandomElement(array) {
+        if (!array.length) {
+            return null;
+        }
+
+        const index = Math.round(Math.random() * (array.length - 1));
+
+        return array[index];
+    },
+
+    getNextPhtoto() {
+        const friend = this.getRandomElement(friendsDB);
+        const photos = photosDB[friend.id];
+        const photo = this.getRandomElement(photos);
+
+        return { friend, url: photo.url}
+    },
 };
