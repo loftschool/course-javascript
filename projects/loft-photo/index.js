2f868620aa101e77153c22fff7c18fd82cc02ab0
Polina <109180069+JimmyCoid@users.noreply.github.com>
2023-07-23 16:00:41 +0300

    работа над проектом 4 неделя

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,10 +1,14 @@
 import pages from './pages';
 import model from './model';
+
+import mainPage from './mainPage';
+import loginPage from './loginPage';
+
 import('./styles.css');
 
 const pageNames = ['login', 'main', 'profile'];
 
-document.addEventListener('click', () => {
-    const pageName = model.getRandomElement(pageNames);
-    pages.openPage(pageName);
-});
+pages.openPage('login');
+loginPage.handleEvents();
+mainPage.handleEvents();
+
