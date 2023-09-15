a7e64c5545fa4fde019dec82317c0a2447a11ae9
Artem Kiselev <kis-ab@yandex.ru>
2023-09-15 13:35:34 +0500

    task-1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(a) {
+  return a;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b=100) {
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +42,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(bar) {
+  return bar();
+}
 
 /*
  Задание 4:
@@ -53,7 +59,14 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(n=0) {
+  function inc() {
+    n++;
+    return n;
+  }
+
+  return inc;
+}
 
 /*
  Задание 5 *:
@@ -64,7 +77,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  return Array.from(arguments)
+}
 
 export {
   returnFirstArgument,
