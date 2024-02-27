7d2ef01afb2c0d4291aa0b375c3b616389b5701b
Vadim Shvetsov <shvetsov-1980@inbox.ru>
2024-02-27 18:59:18 +1000

    готовое дз

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  };
+}
 
 /*
  Задание 2:
@@ -21,7 +25,13 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+function map(array, callback) {
+  let newArray = [...array];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = callback(array[i], i, array);
+  };
+  return newArray;
+}
 
 /*
  Задание 3:
@@ -33,8 +43,19 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
-
+function reduce(array, callback, initial
+) {
+  let startIndex = 0;
+  let all = initial;
+  if (initial === undefined) {
+    startIndex = 1;
+    all = array[0];
+  }
+  for (let i = startIndex; i < array.length; i++) {
+    all = callback(all, array[i], i, array);
+  }
+  return all;
+};
 /*
  Задание 4:
 
@@ -44,6 +65,8 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+function upperProps(obj) {
+  return Object.keys(obj).map(k => k.toLocaleUpperCase());
+}
 
 export { forEach, map, reduce, upperProps };
--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -4,6 +4,15 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    const index = parseInt(Math.random() * (array.length - 1));
+    return array[index];
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB[friends.id];
+    const photo = this.getRandomElement(photos);
+
+    return { friend, url: photo.url };
+  },
 };
