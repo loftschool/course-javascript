a8ce5afcd862c42dc880c8aef7e80a3dd78be202
Vadim Shvetsov <shvetsov-1980@inbox.ru>
2024-03-25 15:16:48 +1000

    готовое дз

--- projects/loft-photo-lite-4/mainPage.js
+++ projects/loft-photo-lite-4/mainPage.js
@@ -9,7 +9,47 @@ export default {
   },
 
   setFriendAndPhoto(friend, id, url) {
+    const photoComp = document.querySelector('.component-photo');
+    const headerPhotoComp = document.querySelector('.component-header-photo');
+    const photoNameComp = document.querySelector('.component-header-name');
+    const footerPhotoComp = document.querySelector('.component-footer-photo');
+
+    this.friend = friend;
+
+    photoComp.style.backgrounImage = `url(${url})`;
+    headerPhotoComp.style.backgrounImage = `url('${frind.photo_50}')`;
+    photoNameComp.innerText = `${fiend.first_name ?? ''} ${frind.last_name ?? ''}`;
+    footerPhotoComp.style.backgrounImage = `url('${model.me.photo_50}')`;
   },
 
   handleEvents() {
+    let startFrom;
+
+    document.querySelector('.component-photo').addEventListener('touchstart', (e) => {
+      e.preventDefault();
+      startFrom = { y: e.changedTouches[0].pageY };
+    });
+
+    document.querySelector('.component-photo').addEventListener('touchend', async (e) => {
+      const direction = e.changedTouches[0].pageY - startFrom.y;
+      if (direction < 0) {
+        await this.getNextPhoto();
+      }
+    });
+
+     document
+       .querySelector('.component-header-profile-link')
+       .addEventListener('click', async () => {
+          await profilePage.setUser(this.friend);
+          pages.openPage('profile');
+       });
+
+      document
+      .querySelector('.component-footer-container-profile-link')
+      .addEventListener('click', async () => {
+        await profilePage.setUser(this.me);
+        pages.openPage('profile');
+      });
+
+  },
 };
--- projects/loft-photo-lite-4/profilePage.js
+++ projects/loft-photo-lite-4/profilePage.js
@@ -3,7 +3,55 @@ import mainPage from './mainPage';
 import pages from './pages';
 
 export default {
-  async setUser(user) {},
+  async setUser(user) {
+    const photoComp = document.querySelector('.component-user-info-photo');
+    const nameComp = document.querySelector('.component-user-info-name');
+    const photosComp = document.querySelector('.component-user-photos');
+    const photos = await model.getPhotos(user.id);
 
-  handleEvents() {},
+    this.user = user;
+
+    photoComp.style.background = `url('${user.photo_100}')`;
+    nameComp.innerText = `${user.first_name  ?? ''} ${user.last_name ?? ''}`;
+    photoComp.innerHTML = '';
+
+    for(const photo of photos.items) {
+      const size = model.findSize(photo);
+      const element = document.createElement('div');
+
+      element.classList.add('component-user-photo');
+      element.dataset.id = photo.id;
+      element.style.backgroundImage = `url(${size.url})`;
+      photoComp.append(element);
+    }
+  },
+
+  handleEvents() {
+    document
+    .querySelector('.component-user-photos')
+    .addEventListener('click', async (e) => {
+      if (e.target.classList.contains('component-user-photo')) {
+        const photoId = e.target.classList.id;
+        const friendPhotos = await model.getPhotos(this.user.id);
+        const photo = friendPhotos.items.find((photo) => photo.id == photoId);
+        const size = model.findSize(photo);
+
+        mainPage.setFriendAndPhoto(this.iser, parseInt(photoId), size.url);
+        pages.openPage('main');
+      }
+    });
+
+    document
+    .querySelector('.page-profile-back')
+    .addEventListener('click', async () => {
+      pages.openPage('main');
+    });
+
+    document
+    .querySelector('.page-profile-exit')
+    .addEventListener('click', async () => {
+      await model.logout();
+      pages.openPage('login');
+    })
+  },
 };
